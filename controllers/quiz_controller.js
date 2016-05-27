d4f0b931772706d68a9b5a0406e84394cdf2c87d
Nacho Galan <nachogr155@gmail.com>
2016-05-27 17:04:23 +0200

    Tema 16: Borrar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -98,3 +98,16 @@ exports.update = function(req, res, next) {
     });
 };
 
+// DELETE /quizzes/:quizId
+exports.destroy = function(req, res, next) {
+    req.quiz.destroy()
+      .then( function() {
+  	  req.flash('success', 'Quiz borrado con éxito.');
+        res.redirect('/quizzes');
+      })
+      .catch(function(error){
+  	  req.flash('error', 'Error al editar el Quiz: '+error.message);
+        next(error);
+      });
+};
+
