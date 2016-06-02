be8522504e8a186781bbd60c3a0413bf1fc21712
Nacho Galan <nachogr155@gmail.com>
2016-06-02 15:54:51 +0200

    Autores de Quizzes

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -2,13 +2,6 @@ var models = require('../models');
 var Sequelize = require('sequelize');
 var url = require('url');
 
-/*
- * Autenticar un usuario: Comprueba si el usuario esta registrado en users
- *
- * Devuelve una Promesa que busca el usuario con el login dado y comprueba su password.
- * Si la autenticacion es correcta, la promesa se satisface devuelve un objeto con el User.
- * Si la autenticacion falla, la promesa se satisface pero devuelve null.
- */
 var authenticate = function(login, password) {
     
     return models.User.findOne({where: {username: login}})
@@ -29,7 +22,6 @@ exports.new = function(req, res, next) {
     var redir = req.query.redir || 
                 url.parse(req.headers.referer || "/").pathname;
 
-    // No volver al formulario de login ni al de registro.
     if (redir === '/session' || redir === '/users/new') {
         redir = "/";
     }
@@ -38,7 +30,6 @@ exports.new = function(req, res, next) {
 };
 
 
-// POST /session   -- Crear la sesion si usuario se autentica
 exports.create = function(req, res, next) {
 
     var redir = req.body.redir || '/'
@@ -49,17 +40,18 @@ exports.create = function(req, res, next) {
     authenticate(login, password)
         .then(function(user) {
             if (user) {
-    	        // Crear req.session.user y guardar campos id y username
-    	        // La sesión se define por la existencia de: req.session.user
-    	        req.session.user = {id:user.id, username:user.username};
-
+             
+                req.session.user = {id:user.id, username:user.username, expiracion:user.expiracion};
+                var date = new Date();
+                date = date.setMinutes(date.getMinutes()+2);
+                req.session.user.expiracion= date;
                 res.redirect(redir); // redirección a redir
             } else {
                 req.flash('error', 'La autenticación ha fallado. Reinténtelo otra vez.');
                 res.redirect("/session?redir="+redir);
             }
-		})
-		.catch(function(error) {
+        })
+        .catch(function(error) {
             req.flash('error', 'Se ha producido un error: ' + error);
             next(error);        
     });
--- models/index.js
+++ models/index.js
@@ -37,6 +37,10 @@ var User = sequelize.import(path.join(__dirname,'user'));
 Comment.belongsTo(Quiz);
 Quiz.hasMany(Comment);
 
+// Relacion 1 a N entre User y Quiz:
+User.hasMany(Quiz, {foreignKey: 'AuthorId'});
+Quiz.belongsTo(User, {as: 'Author', foreignKey: 'AuthorId'});
+
 
 exports.Quiz = Quiz;
 exports.Comment = Comment; // exportar definición de tabla Comments
